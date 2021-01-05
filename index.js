33ea226804ab1a1258a53187ea450355867edce9
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-01-05 15:11:20 -0300

    first test done

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
 const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send({
-        Name: 'ONG do jose',
-        Description: 'doaçao de 10 cestas basicas',
-        Author: 'jose',
-        Date: '01/01/2020'});
-});
 
 app.listen(port, () => {
     console.log(`App listening on localhost:${port}`);
